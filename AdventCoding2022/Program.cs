f852b0bf9679d0b0c677e7316692cb9926879c55
Andrew Mumby <andrew@mumby.info>
2022-12-05 11:46:02 +0000

    Turns out we need to trim a little bit

--- AdventCoding2022/Program.cs
+++ AdventCoding2022/Program.cs
@@ -10,138 +10,139 @@ using System.Windows;
 
 namespace AdventCoding2022
 {
-        class Program
+    class Program
+    {
+        [STAThreadAttribute]
+        static void Main(string[] args)
         {
-            [STAThreadAttribute]
-            static void Main(string[] args)
+            int day = 25;
+            string part = "B";
+            for (day = 25; day > 0; day--)
             {
-                int day = 25;
-                string part = "B";
-                for (day = 25; day > 0; day--)
+                Type searchDayClass = Type.GetType("AdventCoding2022.Day" + day);
+                if (searchDayClass != null)
                 {
-                    Type searchDayClass = Type.GetType("AdventCoding2022.Day" + day);
-                    if (searchDayClass != null)
+                    MethodInfo searchPartMethod = searchDayClass.GetMethod("B");
+                    if (searchPartMethod != null)
                     {
-                        MethodInfo searchPartMethod = searchDayClass.GetMethod("B");
+                        part = "B";
+                        break;
+                    }
+                    else
+                    {
+                        searchPartMethod = searchDayClass.GetMethod("A");
                         if (searchPartMethod != null)
                         {
-                            part = "B";
+                            part = "A";
                             break;
                         }
-                        else
-                        {
-                            searchPartMethod = searchDayClass.GetMethod("A");
-                            if (searchPartMethod != null)
-                            {
-                                part = "A";
-                                break;
-                            }
-                        }
                     }
                 }
+            }
 
-                Console.WriteLine("Enter the day to run, or none for the latest (Day" + day + part + "):");
-                string input = Console.ReadLine();
-                if (input == "party")
+            Console.WriteLine("Enter the day to run, or none for the latest (Day" + day + part + "):");
+            string input = Console.ReadLine();
+            if (input == "party")
+            {
+                for (day = 1; day <= 25; day++)
                 {
-                    for (day = 1; day <= 25; day++)
+                    try
                     {
-                        try
-                        {
-                            part = "A";
-                            string problem = File.ReadAllText("Day" + day + "\\input.txt");
-                            Stopwatch sw = new Stopwatch();
-                            Type dayClass = Type.GetType("AdventCoding2022.Day" + day);
-                            MethodInfo partMethod = dayClass.GetMethod(part);
-                            sw.Start();
-                            string answer = partMethod.Invoke(dayClass, new object[] { problem }).ToString();
-                            sw.Stop();
-                            //Console.Write("Answer: " + answer + " ");
-                            Console.Write("Day " + day + part + ": ");
-                            Console.WriteLine("In: " + Convert.ToDouble(sw.ElapsedMilliseconds) / 1000 + "s");
-
-                            part = "B";
-                            problem = File.ReadAllText("Day" + day + "\\input.txt");
-                            sw = new Stopwatch();
-                            dayClass = Type.GetType("AdventCoding2022.Day" + day);
-                            partMethod = dayClass.GetMethod(part);
-                            sw.Start();
-                            answer = partMethod.Invoke(dayClass, new object[] { problem }).ToString();
-                            sw.Stop();
-                            //Console.Write("Answer: " + answer + " ");
-                            Console.Write("Day " + day + part + ": ");
-                            Console.WriteLine("In: " + Convert.ToDouble(sw.ElapsedMilliseconds) / 1000 + "s");
-                        }
-                        catch (System.IO.DirectoryNotFoundException)
-                        { }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine(e.ToString());
-                        }
-                    }
-                }
-                else
-                {
-                    if (input != "")
-                    {
-                        day = Convert.ToInt32(input.Substring(0, input.Length - 1));
-                        part = input.Substring(input.Length - 1).ToUpper();
-
-                    }
-                    Console.WriteLine("Running code for " + day + part);
-                    Type dayClass = Type.GetType("AdventCoding2022.Day" + day);
-                    if (dayClass != null)
-                    {
+                        part = "A";
+                        string problem = File.ReadAllText("Day" + day + "\\input.txt");
+                        Stopwatch sw = new Stopwatch();
+                        Type dayClass = Type.GetType("AdventCoding2022.Day" + day);
                         MethodInfo partMethod = dayClass.GetMethod(part);
-                        if (partMethod != null)
+                        sw.Start();
+                        string answer = partMethod.Invoke(dayClass, new object[] { problem }).ToString();
+                        sw.Stop();
+                        //Console.Write("Answer: " + answer + " ");
+                        Console.Write("Day " + day + part + ": ");
+                        Console.WriteLine("In: " + Convert.ToDouble(sw.ElapsedMilliseconds) / 1000 + "s");
+
+                        part = "B";
+                        problem = File.ReadAllText("Day" + day + "\\input.txt");
+                        sw = new Stopwatch();
+                        dayClass = Type.GetType("AdventCoding2022.Day" + day);
+                        partMethod = dayClass.GetMethod(part);
+                        sw.Start();
+                        answer = partMethod.Invoke(dayClass, new object[] { problem }).ToString();
+                        sw.Stop();
+                        //Console.Write("Answer: " + answer + " ");
+                        Console.Write("Day " + day + part + ": ");
+                        Console.WriteLine("In: " + Convert.ToDouble(sw.ElapsedMilliseconds) / 1000 + "s");
+                    }
+                    catch (System.IO.DirectoryNotFoundException)
+                    { }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+                }
+            }
+            else
+            {
+                if (input != "")
+                {
+                    day = Convert.ToInt32(input.Substring(0, input.Length - 1));
+                    part = input.Substring(input.Length - 1).ToUpper();
+
+                }
+                Console.WriteLine("Running code for " + day + part);
+                Type dayClass = Type.GetType("AdventCoding2022.Day" + day);
+                if (dayClass != null)
+                {
+                    MethodInfo partMethod = dayClass.GetMethod(part);
+                    if (partMethod != null)
+                    {
+                        // Got the method we're running
+                        // First, run the tests
+                        string[] testFileNames = Directory.GetFiles("Day" + day + "\\" + part);
+                        bool passed = true;
+                        foreach (string testFileName in testFileNames)
                         {
-                            // Got the method we're running
-                            // First, run the tests
-                            string[] testFileNames = Directory.GetFiles("Day" + day + "\\" + part);
-                            bool passed = true;
-                            foreach (string testFileName in testFileNames)
+                            string[] testText = File.ReadAllLines(testFileName);
+                            string correctAnswer = testText[0];
+                            string question = "";
+                            for (int i = 1; i < testText.Length; i++)
                             {
-                                string[] testText = File.ReadAllLines(testFileName);
-                                string correctAnswer = testText[0];
-                                string question = "";
-                                for (int i = 1; i < testText.Length; i++)
-                                {
-                                    question += testText[i] + Environment.NewLine;
-                                }
-                                string actualAnswer = partMethod.Invoke(dayClass, new object[] { question }).ToString();
-                                Console.Write("Test " + testFileName + ": ");
-                                if (actualAnswer.Equals(correctAnswer))
-                                {
-                                    Console.WriteLine("PASSED");
-                                }
-                                else
-                                {
-                                    Console.WriteLine("FAILED! Expected: " + correctAnswer + ", Actual: " + actualAnswer);
-                                    passed = false;
-                                }
+                                question += testText[i] + Environment.NewLine;
                             }
-                            // If all the tests pass, run the problem text
-                            if (passed)
+                            question = question.TrimEnd();
+                            string actualAnswer = partMethod.Invoke(dayClass, new object[] { question }).ToString();
+                            Console.Write("Test " + testFileName + ": ");
+                            if (actualAnswer.Equals(correctAnswer))
                             {
-                                string problem = File.ReadAllText("Day" + day + "\\input.txt");
-                                Stopwatch sw = new Stopwatch();
-                                sw.Start();
-                                string answer = partMethod.Invoke(dayClass, new object[] { problem }).ToString();
-                                sw.Stop();
-                                Console.WriteLine("Answer: " + answer);
-                                Console.WriteLine("In: " + Convert.ToDouble(sw.ElapsedMilliseconds) / 1000 + "s");
-                                Clipboard.SetText(answer);
+                                Console.WriteLine("PASSED");
                             }
                             else
                             {
-                                Console.WriteLine("Tests failed. Aborting");
+                                Console.WriteLine("FAILED! Expected: " + correctAnswer + ", Actual: " + actualAnswer);
+                                passed = false;
                             }
                         }
-
+                        // If all the tests pass, run the problem text
+                        if (passed)
+                        {
+                            string problem = File.ReadAllText("Day" + day + "\\input.txt");
+                            Stopwatch sw = new Stopwatch();
+                            sw.Start();
+                            string answer = partMethod.Invoke(dayClass, new object[] { problem }).ToString();
+                            sw.Stop();
+                            Console.WriteLine("Answer: " + answer);
+                            Console.WriteLine("In: " + Convert.ToDouble(sw.ElapsedMilliseconds) / 1000 + "s");
+                            Clipboard.SetText(answer);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Tests failed. Aborting");
+                        }
                     }
-                }
-                Console.ReadLine();
-            }
-        }
 
+                }
+            }
+            Console.ReadLine();
+        }
     }
+
+}
